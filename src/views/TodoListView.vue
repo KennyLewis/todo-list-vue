2e77621852e08c265a1b7077f789b9726b4502af
Kenny Lewis <kl569@nau.edu>
2025-02-06 12:47:29 -0700

    Stub out the Todo List View page (#1)

--- src/views/TodoListView.vue
+++ src/views/TodoListView.vue
@@ -2,5 +2,55 @@
   <div>
     <h1>Todo List</h1>
     <input class="form-control" type="text" placeholder="Add Todo" />
+    <ul class="mt-2">
+      <li v-for="todo in todos" :key="todo.id">
+        <input type="checkbox" :id="todo.id" v-model="todo.completed" @click="toggleCompleted(todo.id)" />
+        <label class="pl-2" :for="todo.id" :class="{ strikethrough: todo.completed }">{{ todo.title }}</label>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script lang="ts">
+const initialTodos = [
+  { id: 1, title: 'Learn Vue 3', completed: false },
+  { id: 2, title: 'Learn React', completed: true },
+  { id: 3, title: 'Build a project', completed: false },
+]
+
+export default {
+  name: 'TodoListView',
+  data() {
+    return {
+      todos: initialTodos,
+    }
+  },
+  methods: {
+    toggleCompleted(id: number) {
+      const todo = this.todos.find((todo) => todo.id === id)
+      if (todo) {
+        todo.completed = !todo.completed
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.mt-2 {
+  margin-top: 0.5rem;
+}
+
+.pl-2 {
+  padding-left: 0.5rem;
+}
+
+.strikethrough {
+  text-decoration: line-through;
+}
+</style>
