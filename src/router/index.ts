9c6ebfd7717df5893820137145e674c73ceea2b3
Kenny Lewis <kl569@nau.edu>
2025-02-06 12:29:02 -0700

    Stub out page

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/todo-list',
+      name: 'todo-list',
+      component: () => import('@/views/TodoListView.vue'),
+    },
   ],
 })
 
---
+++ src/views/TodoListView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <input class="form-control" type="text" placeholder="Add Todo" />
+  </div>
+</template>
