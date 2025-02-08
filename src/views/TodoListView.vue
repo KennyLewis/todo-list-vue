089bde74458e18ac5e830e4f8fc3c3c27735861a
Kenny Lewis <kl569@nau.edu>
2025-02-08 11:25:47 -0700

    Implement add, delete completed, and local storage features (#3)
    * Start of local storage functionality
    
    * Fix some naming, fix types
    
    * Implement basic add functionality
    
    * Implement deleteCompletedTodoItems

--- src/views/TodoListView.vue
+++ src/views/TodoListView.vue
@@ -1,41 +1,83 @@
 <template>
   <div>
     <h1>Todo List</h1>
-    <input class="form-control" type="text" placeholder="Add Todo" />
+    <input type="text" v-model="todoInput" @keyup.enter="addTodoItem(todoInput)" />
+    <button @click="addTodoItem(todoInput)">Add</button>
     <ul class="mt-2">
       <li v-for="todoItem in todoItems" :key="todoItem.id">
         <input type="checkbox" :id="todoItem.id" v-model="todoItem.completed" @click="toggleCompleted(todoItem.id)" />
         <label for="todo.id" :class="{ strikethrough: todoItem.completed }">{{ todoItem.title }}</label>
-        <button @click="deleteTodo(todoItem.id)">Delete</button>
+        <button @click="deleteTodoItem(todoItem.id)">Delete</button>
       </li>
     </ul>
+    <button @click="deleteCompletedTodoItems()">Delete Completed Todo Items</button>
   </div>
 </template>
 
 <script lang="ts">
-const initialTodoItems = [
-  { id: 1, title: 'Learn Vue 3', completed: false },
-  { id: 2, title: 'Learn React', completed: true },
-  { id: 3, title: 'Build a project', completed: false },
+
+import { v4 as uuidv4 } from 'uuid';
+
+interface TodoItem {
+  id: string
+  title: string
+  completed: boolean
+}
+
+const initialTodoItems: TodoItem[] = [
+  { id: uuidv4(), title: 'Learn Vue 3', completed: false },
+  { id: uuidv4(), title: 'Learn React', completed: true },
+  { id: uuidv4(), title: 'Build a project', completed: false },
 ]
+
+const todoInput = '';
 
 export default {
   name: 'TodoListView',
   data() {
+    const todoItems = this.getLocalStorage() as TodoItem[];
     return {
-      todoItems: initialTodoItems,
+      todoInput: todoInput,
+      todoItems: todoItems
     }
   },
   methods: {
-    toggleCompleted(id: number) {
-      const todo = this.todoItems.find((todo) => todo.id === id)
+    addTodoItem(title: string) {
+      if (!title) {
+        return;
+      }
+
+      const id = uuidv4();
+      this.todoItems.push({ id, title, completed: false });
+      this.todoInput = '';
+      this.saveLocalStorage();
+    },
+    deleteTodoItem(id: string) {
+      this.todoItems = this.todoItems.filter((todoItem) => todoItem.id !== id);
+      this.saveLocalStorage();
+    },
+    deleteCompletedTodoItems() {
+      this.todoItems = this.todoItems.filter((todoInput) => !todoInput.completed);
+      this.saveLocalStorage();
+    },
+    toggleCompleted(id: string) {
+      const todo = this.todoItems.find((todoItem) => todoItem.id === id)
       if (todo) {
         todo.completed = !todo.completed
       }
+      this.saveLocalStorage();
     },
-    deleteTodo(id: number) {
-      this.todoItems = this.todoItems.filter((todo) => todo.id !== id)
-    }
+    getLocalStorage(): TodoItem[] {
+      const todoItems = localStorage.getItem('todoItems')
+      if (todoItems) {
+        return JSON.parse(todoItems);
+      }
+
+      return initialTodoItems;
+    },
+    saveLocalStorage() {
+      localStorage.setItem('todoItems', JSON.stringify(this.todoItems))
+    },
   },
 }
 </script>
